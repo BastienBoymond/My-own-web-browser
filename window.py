275460e1c77a06be7357bf45f8483c400b94d138
Bastien <bastien.boymond@epitech.eu>
2022-04-06 08:31:49 +0200

    Change name of tabs on load and on tabs switch

--- window.py
+++ window.py
@@ -30,9 +30,17 @@ class MainWindow(QMainWindow):
         reload_btn = QAction(QIcon('./assets/reload.png'),'Reload', self)
         reload_btn.triggered.connect(self.tabs.currentWidget().reload)
 
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+
         add_btn = QAction(QIcon('./assets/plus.png'), 'Add', self)
         add_btn.triggered.connect(lambda: self.add_new_tab())
-        self.navbar.addActions([back_btn, forward_btn, reload_btn, add_btn])
+        
+        self.navbar.addActions([back_btn, forward_btn, add_btn, reload_btn])
+        self.navbar.addWidget(self.url_bar)
+
+        self.tabs.currentWidget().urlChanged.connect(self.update_url)
+        self.tabs.currentChanged.connect( self.current_tab_changed )
 
 
     def add_new_tab(self, qurl=None, label="Blank"):
@@ -46,8 +54,22 @@ class MainWindow(QMainWindow):
 
         self.tabs.setCurrentIndex(i)
 
+        browser.urlChanged.connect( lambda qurl, browser=browser:
+                self.url_bar.setText(qurl.toString()))
 
+        browser.loadFinished.connect( lambda _, i=i, browser=browser:
+            self.tabs.setTabText(i, browser.page().title()) )
 
     def close_current_tab(self, i):
         self.tabs.removeTab(i)
 
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.tabs.currentWidget().setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
+
+    def current_tab_changed(self, i):
+        qurl = self.tabs.currentWidget().url()
+        self.url_bar.setText(qurl.toString())
